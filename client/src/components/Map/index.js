722d351a98fd2d04a8d6e018de75ff9a62928a23
JeremyF <jfulbright@gmail.com>
2023-02-01 21:07:39 -0700

    Added functionality to display geo location on a Goolge Map in cart

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,37 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-const MapContainer = ({ google }) => {
-  const [position, setPosition] = useState({});
+// Display Google Map location in component
+const MapContainer = (props) => {
+  // const [position, setPosition] = useState({});
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setPosition(position.coords);
-      },
-      (error) => {
-        console.error(error);
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-    console.log(position);
-  }, []);
+  // useEffect(() => {
+  //   setPosition(props.mapContainer);
+  // }, []);
 
+  console.log(props);
   return (
     <Map
-      google={google}
+      google={props.google}
       zoom={14}
-      style={{ width: "100%", height: "100%" }}
+      style={{ width: "50%", height: "50%" }}
       initialCenter={{
-        lat: position.lat || 37.7749,
-        lng: position.lng || -122.4194,
+        lat: props.mapContainer.lat || 37.7749,
+        lng: props.mapContainer.lng || -122.4194,
       }}
     >
-      <Marker position={{ lat: position.lat, lng: position.lng }} />
+      <Marker
+        position={{ lat: props.mapContainer.lat, lng: props.mapContainer.lng }}
+      />
     </Map>
   );
 };
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyDm4eD7fTLayP9ovoSx_oilbzHPb4e81Cs",
+  apiKey: "AIzaSyAiiBRt4ChUhgVDQ1PXmJHkS2ZDoJE9X-s",
 })(MapContainer);
